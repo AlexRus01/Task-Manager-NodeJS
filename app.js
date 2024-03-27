91150b20915e0bad4ab04ddc8e4c84cd155e087f
AlexRus01 <alex.rus11111@gmail.com>
2024-03-27 21:07:00 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+//Initializam serverul express
+const express = require('express')
+const app = express()
+
+// ruta
+
+app.get('/hello', (req, res) => {
+    res.send('Task Manager App')
+    
+})
+
+
+const port = 3000
+
+app.listen(port, console.log(`Serverul functioneaza pe portul ${port}...`))
